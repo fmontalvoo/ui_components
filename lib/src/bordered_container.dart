94886c43f48af775444b27c43cf94ac0f35b0652
Frank Montalvo Ochoa <fmontalvoo@ivortec.com>
2020-08-20 13:48:15 -0500

    Fix stroke width.

--- lib/src/bordered_container.dart
+++ lib/src/bordered_container.dart
@@ -29,7 +29,7 @@ class BorderedContainer extends StatelessWidget {
           spreadRadius: 3.0)
     ],
     this.child,
-    this.strokeWidth = 10.0,
+    this.strokeWidth = 1.0,
   }) : super(key: key);
 
   @override
